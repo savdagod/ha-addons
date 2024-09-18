186f3ac930de34dced4374c002e1368cd749c6b4
Bruce <107087485+savdagod@users.noreply.github.com>
2024-09-18 00:50:09 -0400

    Update TidbytText.sh

--- TidbytAssistant-dev/TidbytText.sh
+++ TidbytAssistant-dev/TidbytText.sh
@@ -12,13 +12,13 @@ ROOT_DIR=/tmp
 FILE=text-$TEXT_TYPE
 
 cp /opt/display/$FILE.star $ROOT_DIR/$FILE.star -f
-perl -pi -e "s/%DISPLAY_TEXT%/$CONTENT/g" $ROOT_DIR/$FILE.star
-perl -pi -e "s/%DISPLAY_FONT%/$FONT/g" $ROOT_DIR/$FILE.star
-perl -pi -e "s/%DISPLAY_COLOR%/$COLOR/g" $ROOT_DIR/$FILE.star
+#perl -pi -e "s/%DISPLAY_TEXT%/$CONTENT/g" $ROOT_DIR/$FILE.star
+#perl -pi -e "s/%DISPLAY_FONT%/$FONT/g" $ROOT_DIR/$FILE.star
+#perl -pi -e "s/%DISPLAY_COLOR%/$COLOR/g" $ROOT_DIR/$FILE.star
 
 RENDER_PATH=/tmp/render.webp
 
-/usr/local/bin/pixlet render $ROOT_DIR/$FILE.star -o $RENDER_PATH
+/usr/local/bin/pixlet render $ROOT_DIR/$FILE.star -o $RENDER_PATH -content $CONTENT -font $FONT -color $COLOR
 
 /usr/local/bin/pixlet push --api-token $TB_TOKEN $TB_DEVICEID $RENDER_PATH
 rm -r /tmp/*
