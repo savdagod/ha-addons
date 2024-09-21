70171a00efa2ff33bc58c6d442d41fb8c9958c03
Bruce <bjt.homelab@gmail.com>
2024-09-20 23:12:03 -0400

    update files

---
+++ TidbytAssistant-dev/display/text-title.star
@@ -0,0 +1,32 @@
+load("render.star", "render")
+
+def main(config):
+	content = config.get("content")
+	title = config.get("title)
+	font = config.get("font")
+	titlefont = config.get("titlefont")
+	color = config.get("color")
+	titlecolor = config.get("titlecolor")
+    return render.Root(
+        child = render.Column(
+            expanded = True,
+            main_align = "center",
+            cross_align = "center",
+            children = [
+                render.Text(
+					content=title, 
+					font=titlefont,
+					color=titlecolor,
+				),
+				render.Marquee(
+					width=60,
+					offset_start=59,
+					child=render.Text(
+						content=content,
+						font=font,
+						color=color,
+					),
+				),
+            ],
+        ),
+    )
--- TidbytAssistant-dev/scripts/TidbytText.sh
+++ TidbytAssistant-dev/scripts/TidbytText.sh
@@ -7,6 +7,9 @@ TB_TOKEN=${3:?"missing arg 3 for TOKEN"}
 TEXT_TYPE=${4:?"missing arg 4 for TEXT_TYPE"}
 FONT=${5:?"missing arg 5 for FONT"}
 COLOR=${6:?"missing arg 6 for COLOR"}
+TITLE=${7:-}
+TITLE_COLOR=${8:-}
+TITLE_FONT=${9:-}
 
 ROOT_DIR=/tmp
 FILE=text-$TEXT_TYPE
@@ -15,7 +18,11 @@ cp /opt/display/$FILE.star $ROOT_DIR/$FILE.star -f
 
 RENDER_PATH=/tmp/render.webp
 
-/usr/local/bin/pixlet render $ROOT_DIR/$FILE.star content="$CONTENT" font="$FONT" color="$COLOR" -o $RENDER_PATH
+if [[ "$TEXT_TYPE" == "title" ]]; then
+	/usr/local/bin/pixlet render $ROOT_DIR/$FILE.star content="$CONTENT" font="$FONT" color="$COLOR" title="$TITLE" titlecolor="$TITLE_COLOR" titlefont="$TITLE_FONT" -o $RENDER_PATH
+else;
+	/usr/local/bin/pixlet render $ROOT_DIR/$FILE.star content="$CONTENT" font="$FONT" color="$COLOR" -o $RENDER_PATH
+fi 
 
 /usr/local/bin/pixlet push --api-token $TB_TOKEN $TB_DEVICEID $RENDER_PATH
 rm -r /tmp/*
