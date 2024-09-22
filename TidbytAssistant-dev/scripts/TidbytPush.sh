154208b49c7926c81bc8a3fa56daef83b4affcf6
Bruce <107087485+savdagod@users.noreply.github.com>
2024-09-22 18:31:43 -0400

    Update TidbytPush.sh

--- TidbytAssistant-dev/scripts/TidbytPush.sh
+++ TidbytAssistant-dev/scripts/TidbytPush.sh
@@ -21,7 +21,7 @@ RENDER_PATH=/tmp/render.webp
 
 cp $ROOT_DIR/$CONTENT.star /tmp/$CONTENT.star -f
 if [[ $ARGS ]]; then
-        ARRAY=($ARGS)
+        ARRAY=$ARGS
 	/usr/local/bin/pixlet render /tmp/$CONTENT.star "${ARRAY[*]}" -o $RENDER_PATH
 else
 	/usr/local/bin/pixlet render /tmp/$CONTENT.star -o $RENDER_PATH
