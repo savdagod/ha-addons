470e1ab4a804ab35424a52c3183263b84abe48fe
Bruce <bjt.homelab@gmail.com>
2024-09-22 15:40:31 -0400

    update files

--- TidbytAssistant-dev/scripts/TidbytPush.sh
+++ TidbytAssistant-dev/scripts/TidbytPush.sh
@@ -6,6 +6,7 @@ CONTENT=${1:?"missing arg 1 for CONTENT"}
 TB_DEVICEID=${2:?"missing arg 2 for DEVICE ID"}
 TB_TOKEN=${3:?"missing arg 3 for TOKEN"}
 CONTENT_TYPE=${4:?"missing arg 4 for CONTENT_TYPE"}
+ARGS=${5:-}
 
 rm -f /tmp/*
 
@@ -19,7 +20,11 @@ esac
 RENDER_PATH=/tmp/render.webp
 
 cp $ROOT_DIR/$CONTENT.star /tmp/$CONTENT.star -f
-/usr/local/bin/pixlet render /tmp/$CONTENT.star -o $RENDER_PATH
+if [[ $ARGS ]]; then
+	/usr/local/bin/pixlet render /tmp/$CONTENT.star $ARGS -o $RENDER_PATH
+else
+	/usr/local/bin/pixlet render /tmp/$CONTENT.star -o $RENDER_PATH
+fi
 
 /usr/local/bin/pixlet push --api-token $TB_TOKEN $TB_DEVICEID $RENDER_PATH
 
