863ca43b22ded14f7d0fb5a76b6126a432566ff4
Bruce <bjt.homelab@gmail.com>
2024-09-22 22:09:09 -0400

    Update TidbytPublish.sh

--- TidbytAssistant-dev/scripts/TidbytPublish.sh
+++ TidbytAssistant-dev/scripts/TidbytPublish.sh
@@ -8,14 +8,23 @@ TB_TOKEN=${3:?"missing arg 3 for TOKEN"}
 CONTENT_ID=${4:?"missing arg 4 for CONTENT_ID"}
 ARGS=${5:-}
 
-
 rm -f /tmp/*
 
 ROOT_DIR=/homeassistant/tidbyt
 RENDER_PATH=/tmp/render.webp
 
 cp $ROOT_DIR/$CONTENT.star /tmp/$CONTENT.star -f
-/usr/local/bin/pixlet render /tmp/$CONTENT.star -o $RENDER_PATH
+if [[ $ARGS ]]; then
+    arg=()
+    IFS=';' read -ra pairs <<< "$ARGS"
+    for pair in "${pairs[@]}"; do
+        IFS='=' read -r key value <<< "$pair"
+        arg+=("$key=$value")
+    done
+	/usr/local/bin/pixlet render /tmp/$CONTENT.star "${arg[@]}" -o $RENDER_PATH
+else
+	/usr/local/bin/pixlet render /tmp/$CONTENT.star -o $RENDER_PATH
+fi
 
 /usr/local/bin/pixlet push --installation-id $CONTENT_ID --background --api-token $TB_TOKEN $TB_DEVICEID $RENDER_PATH
 
